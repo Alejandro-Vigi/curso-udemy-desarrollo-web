2a8ba57fd6ad24f67eb01d3f10ded4b452c3e85b
Alejandro-Vigi <marco.vigi@ingenieria.unam.edu>
2024-12-10 11:37:04 -0600

    Variables en JS con var

---
+++ IntroduccionJS/js/01.js
@@ -0,0 +1,24 @@
+// Variables
+
+var producto = 'Audifonos Gamer'; // Iniciar variable y asignarle valor.
+
+var disponible;  // Iniciar variable sin valor.
+
+// producto = true; // Reasignando el valor de la variable
+
+disponible = true; // Reasignando el valor de la variable
+
+var producto1 = 'Computadora',
+    disponible1 = true,
+    categoria = 'Computadoras';
+
+// var 1disponible
+var disponible_;
+
+// Estilos para las variables
+var nombre_producto = 'Monitor HD'; // underscore
+var nombreProducto = 'Monitor HD'; // Camelcase
+var NombreProducto = 'Monitor HD'; // Pascalcase
+var nombreproducto = 'Monitor HD';
+
+console.log(producto)
