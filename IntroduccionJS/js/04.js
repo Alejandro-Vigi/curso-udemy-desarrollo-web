740b1630e1afa97f594ca26fd21fb004305d1958
Alejandro-Vigi <marco.vigi@ingenieria.unam.edu>
2024-12-10 11:52:25 -0600

    Strings o Cadenas de Texto

---
+++ IntroduccionJS/js/04.js
@@ -0,0 +1,18 @@
+// String o cadena de texto
+const producto = "Monitor de 20 pulgadas";
+const producto2 = String('Monitor de 30 pulgadas');
+const producto3 = new String('Monitor de 50 pulgadas');
+
+
+console.log(producto);
+console.log(producto2);
+console.log(producto3);
+console.log(typeof producto3);
+
+// typeof -> saber tipo de dato
+
+// Se puede usar comillas sencillas '' o dobles ""
+
+// Mandar comillas dobles al strink 
+const producto4 = "Monitor de 20\""; // Comillas dobles
+const producto5 = 'Monitor de 20"';  // Comillas sencillas
