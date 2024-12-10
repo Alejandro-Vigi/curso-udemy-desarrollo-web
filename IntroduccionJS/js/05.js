817b54d5027201ed0bb060ef98d6c00d6e9e349a
Alejandro-Vigi <marco.vigi@ingenieria.unam.edu>
2024-12-10 12:12:58 -0600

    Numeros y Operadores en JS

---
+++ IntroduccionJS/js/05.js
@@ -0,0 +1,19 @@
+// Numeros
+
+// No hay flotantes o enteros, todos son tratados como numeros
+const numero1 = 100;
+const numero2 = 200;
+const numero3 = 7;
+const numero4 = 6;
+const numero5 = -5;
+
+console.log(numero1 + numero2);
+console.log(numero2 - numero1);
+console.log(numero2 * numero1);
+console.log(numero2 / numero1);
+console.log(numero3 % numero4);
+
+const cliente = "Alejandro";
+const cliente2 = "Pablo";
+
+console.log(cliente * cliente2)
