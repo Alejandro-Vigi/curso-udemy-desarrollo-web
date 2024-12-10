fc2704364a96f576960f9b188bb25405e7d7e6fd
Alejandro-Vigi <marco.vigi@ingenieria.unam.edu>
2024-12-10 12:00:47 -0600

    Agregando metodos para los Strings

--- IntroduccionJS/js/04.js
+++ IntroduccionJS/js/04.js
@@ -3,7 +3,7 @@ const producto = "Monitor de 20 pulgadas";
 const producto2 = String('Monitor de 30 pulgadas');
 const producto3 = new String('Monitor de 50 pulgadas');
 
-
+// Ver en la consola la impresion
 console.log(producto);
 console.log(producto2);
 console.log(producto3);
@@ -16,3 +16,18 @@ console.log(typeof producto3);
 // Mandar comillas dobles al strink 
 const producto4 = "Monitor de 20\""; // Comillas dobles
 const producto5 = 'Monitor de 20"';  // Comillas sencillas
+
+// Métodos para los strings
+const tweet = 'Aprendiendo JS con el curso de Desarrollo Web completo';
+const producto7 = 'Monitor HD"';
+
+console.log(tweet.length);
+console.log(producto7);
+
+// IndexOf Que posicion se encuentra un texto en un String
+console.log(tweet.indexOf('JS'));
+console.log(producto7.indexOf('Tablet'));
+
+// Includes (Returna True o False si encuentra un texto en un String
+console.log(tweet.includes('JS'));
+console.log(producto7.includes('Tablet'));
