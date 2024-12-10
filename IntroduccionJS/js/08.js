4749dc021c6a55937fe9d6e55d278979c4834c7d
Alejandro-Vigi <marco.vigi@ingenieria.unam.edu>
2024-12-10 13:12:09 -0600

    Haciendo concatenacion normal y con Template Strings

---
+++ IntroduccionJS/js/08.js
@@ -0,0 +1,9 @@
+const nombre = "Alejandro";
+const email = "correo@correo.com"
+
+// Concatenacion
+console.log("Nombre Cliente: " + nombre + " Email: " + email); // Forma Antigua
+console.log("Nombre Cliente:" , nombre , "Email:" , email); // Forma Antigua
+
+// Template Strings - Strings Literals
+console.log(`Nombre Cliente: ${nombre} Email: ${email}`);
