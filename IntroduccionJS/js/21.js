36b7dd12b350057fe4a3e4e6de70b5ccf712c9cb
Alejandro-Vigi <marco.vigi@ingenieria.unam.edu>
2024-12-12 12:42:04 -0600

    Aprendiendo a usar metodos de propiedad

---
+++ IntroduccionJS/js/21.js
@@ -0,0 +1,26 @@
+// Metodos de propiedad (Funciones)
+
+const reproductor = {
+    reproducir : function(id){
+        console.log(`Reproduciendo la cancion con el ID: ${id}`);
+    },
+    pausar : function(){
+        console.log("Pausando...");
+    },
+    crearPlaylist : function(nombre){
+        console.log(`Creando la Playlist:  ${nombre}`);
+    },
+    reproducirPlaylist : function(nombre){
+        console.log(`Reproduciendo la Playlist: ${nombre}`);
+    },
+}
+
+reproductor.borrarCancion = function(id) {
+    console.log(`Eliminando la canción: ${id}`);
+}
+
+reproductor.reproducir(3840);
+reproductor.pausar();
+reproductor.borrarCancion(4887);
+reproductor.crearPlaylist("Heavy Metal");
+reproductor.reproducirPlaylist("Heavy Metal");
