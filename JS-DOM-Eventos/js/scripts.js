632eecbddc90481e6a61abbac582f4adbd7ea4a6
Alejandro-Vigi <marco.vigi@ingenieria.unam.edu>
2024-12-15 17:48:20 -0600

    Aprendiendo a seleccionar elementos HTML con querySelectorAll y getElementById en JS

--- JS-DOM-Eventos/js/scripts.js
+++ JS-DOM-Eventos/js/scripts.js
@@ -1,16 +1,21 @@
 // querySelector
 
-const heading = document.querySelector('.header__texto h2') // Retorna 0 o un elemento
+const heading = document.querySelector('.header__texto h2') // Retorna 0 o 1 elemento
 heading.textContent = 'Nuevo Heading';
 heading.classList.add('nueva-clase');
 console.log(heading);
 
-
-
 // querySelectorAll
+const enlaces = document.querySelectorAll('.navegacion a') // Retorna 0 o todos los elementos con ese selector
+enlaces[0].textContent = 'Nuevo texto para Enlace';
+enlaces[0].href = 'http://google.com';
+enlaces[0].classList.add('nueva-clase2');
+enlaces[0].classList.remove('navegacion__enlace')
 
-
-
-
+// Se puede hacer de la misma forma:
+// document.querySelectorAll('.navegacion a')[0].textContent = 'Nuevo texto para Enlace';
 
 // gerElementById
+
+const heading2 = document.getElementById('heading');
+console.log(heading2);
